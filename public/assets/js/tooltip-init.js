6b73c76454bbc38aae5c25df36f8224a801c7acb
Tasherul Islam <tasherulislam@gmail.com>
2022-10-20 13:33:17 +0600

    update 20-10-2022

---
+++ public/assets/js/tooltip-init.js
@@ -0,0 +1,13 @@
+(function($) {
+	"use strict";
+	var tooltip_init = {
+		init: function() {
+			$("button").tooltip();
+			$("a").tooltip();
+			$("input").tooltip();
+			$("img").tooltip();
+			$(".mytooltip").tooltip();
+		}
+	};
+    tooltip_init.init()
+})(jQuery);
---
+++ resources/views/layout/_head.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="{{asset('public/assets/images/favicon.png')}}" type="image/x-icon">
+    <link rel="shortcut icon" href="{{asset('public/assets/images/favicon.png')}}" type="image/x-icon">
+
+    <title>@yield('title')</title>
+
+    @yield('header_links')
+</head>
